6f8391dbbe18879298218704cb8bd5c75b4eec09
koibaiyang <2586056022@qq.com>
2023-06-28 22:30:08 +0800

    Layout 页面布局

--- src/views/Layout/index.vue
+++ src/views/Layout/index.vue
@@ -1,6 +1,12 @@
+<script setup>
+import LayoutNav from './components/LayoutNav.vue'
+import LayoutHeader from './components/LayoutHeader.vue'
+import LayoutFooter from './components/LayoutFooter.vue'
+</script>
+
 <template>
-  <div>
-    我是一级路由主页
-    <router-view></router-view>
-  </div>
+  <LayoutNav />
+  <LayoutHeader />
+  <RouterView />
+  <LayoutFooter />
 </template>
