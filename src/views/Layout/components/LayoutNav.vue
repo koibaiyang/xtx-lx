603368a9d45a39ee78e375dd7a75ca82de7d2c8e
koibaiyang <2586056022@qq.com>
2023-07-15 21:22:33 +0800

    会员中心路由配置及渲染

--- src/views/Layout/components/LayoutNav.vue
+++ src/views/Layout/components/LayoutNav.vue
@@ -27,7 +27,7 @@ const confirm = () => {
             </el-popconfirm>
           </li>
           <li><a href="javascript:;">我的订单</a></li>
-          <li><a href="javascript:;">会员中心</a></li>
+          <li><a href="javascript:;" @click="$router.push('/member')">会员中心</a></li>
         </template>
         <template v-else>
           <!-- <li><router-link to="/login">测试</router-link></li> -->
